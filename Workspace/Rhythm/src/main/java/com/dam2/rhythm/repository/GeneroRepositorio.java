fe5291614d0293f724cb820da21bc30cfa098668
hmar321 <99360858+hmar321@users.noreply.github.com>
2023-02-14 14:24:15 +0100

    Delete Workspace/Rhythm directory

--- Workspace/Rhythm/src/main/java/com/dam2/rhythm/repository/GeneroRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.dam2.rhythm.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dam2.rhythm.model.Genero;
-
-public interface GeneroRepositorio extends CrudRepository<Genero, Integer> {
-	Genero findByNombre(String nombre);
-}
--- Workspace/Rhythm/src/main/java/com/dam2/rhythm/repository/RolRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.dam2.rhythm.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dam2.rhythm.model.Rol;
-
-public interface RolRepositorio extends CrudRepository<Rol, Integer> {
-	Rol findByNombre(String nombre);
-}
