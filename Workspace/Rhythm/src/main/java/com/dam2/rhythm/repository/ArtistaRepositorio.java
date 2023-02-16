bf48bf5837e5f1820b421b425246a5632b741c5d
hmar321 <99360858+hmar321@users.noreply.github.com>
2023-02-16 14:36:59 +0100

    Delete Workspace/Rhythm directory

--- Workspace/Rhythm/src/main/java/com/dam2/rhythm/repository/ArtistaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.dam2.rhythm.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dam2.rhythm.model.Artista;
-
-public interface ArtistaRepositorio extends CrudRepository<Artista, Integer> {
-	Artista findByNick(String nick);
-}
--- Workspace/Rhythm/src/main/java/com/dam2/rhythm/repository/UsuarioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.dam2.rhythm.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dam2.rhythm.model.Usuario;
-
-public interface UsuarioRepositorio extends CrudRepository<Usuario, Integer> {
-	Usuario findByEmail(String email);
-}
