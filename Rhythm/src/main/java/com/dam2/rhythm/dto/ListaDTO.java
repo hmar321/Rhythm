0c19d9bbe5cf3c75ed67ac607230a9fca93ef81d
hmar321 <99360858+hmar321@users.noreply.github.com>
2023-02-28 03:24:05 +0100

    Delete Rhythm directory

--- Rhythm/src/main/java/com/dam2/rhythm/dto/ListaDTO.java
+++
@@ -1,9 +0,0 @@
-package com.dam2.rhythm.dto;
-
-import java.util.List;
-
-import com.dam2.rhythm.model.Musica;
-
-public class ListaDTO {
-
-}
--- Rhythm/src/main/java/com/dam2/rhythm/repository/AlbumRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.dam2.rhythm.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dam2.rhythm.model.Album;
-
-public interface AlbumRepositorio extends CrudRepository<Album, Integer> {
-
-}
